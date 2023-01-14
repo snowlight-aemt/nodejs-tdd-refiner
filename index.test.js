9f4fd5f21518e38d4a56827d34bd482868c18538
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2023-01-14 11:35:08 +0900

    테스트 코드 정리

--- index.test.js
+++ index.test.js
@@ -1,17 +1,15 @@
 const sut = require('./index');
 // sut: System Under Test
 
-test('sut transforms "hello  world" to "hello world"', () => {
-    const actual = sut("hello  world");
-    expect(actual).toBe("hello world");
+test('stu correctly works' , () => {
+    for (const source of ['hello  world', 'hello   world', 'hello    world']) {
+        const actual = sut(source);
+        expect(actual).toBe("hello world");
+    }
 });
 
-test('sut transforms "hello   world" to "hello world"', () => {
-    const actual = sut("hello   world");
-    expect(actual).toBe("hello world");
-});
+// TODO
+// * 중복을 제거했지만, 어느 소스에서 발생한 에러인지 확인 힘들다
+// * 중간에 테스트가 실패하면 마지막까지 테스트를 확인하지 않는다.
 
-test('sut transforms "hello    world" to "hello world"', () => {
-    const actual = sut("hello    world");
-    expect(actual).toBe("hello world");
-});
+// 결론 : 테스트 피드백이 줄어 들었다. 중복을 제거한다고 무조건 좋은것은 아니다.
